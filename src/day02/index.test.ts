bd914151319f31df4d424175e8b79a1193d51475
A. Fox <anthonyfox1988@gmail.com>
2022-12-02 08:17:43 +0000

    day 02 part 1 and 2

--- src/day02/index.test.ts
+++ src/day02/index.test.ts
@@ -1,5 +1,39 @@
+import {
+  calculateTotalScore,
+  calculateTotalScoreWithInstruction,
+  getChoice,
+  scoreRound,
+} from "."
+
 describe("day02", () => {
-  test("nothing", () => {
-    expect(true).toEqual(false)
+  const testInput = `
+A Y
+B X
+C Z`
+    .trim()
+    .split("\n")
+
+  test("part 1", () => {
+    // win
+    expect(scoreRound("A", "Y")).toEqual(8)
+    // loss
+    expect(scoreRound("B", "X")).toEqual(1)
+    //draw
+    expect(scoreRound("C", "Z")).toEqual(6)
+
+    // score
+    expect(calculateTotalScore(testInput)).toEqual(15)
+  })
+
+  test("part 2", () => {
+    // draw
+    expect(getChoice("A", "Y")).toEqual("X")
+    // lose
+    expect(getChoice("B", "X")).toEqual("X")
+    // win
+    expect(getChoice("C", "Z")).toEqual("X")
+
+    // total score
+    expect(calculateTotalScoreWithInstruction(testInput)).toEqual(12)
   })
 })
--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,8 +1,90 @@
+import { readFileSync } from "fs"
+
+type choiceA = "A" | "B" | "C"
+type choiceB = "X" | "Y" | "Z"
+
+const points = new Map<choiceA | choiceB, number>([
+  ["A", 1],
+  ["B", 2],
+  ["C", 3],
+  ["X", 1],
+  ["Y", 2],
+  ["Z", 3],
+])
+
+const wins = new Map<choiceA, choiceB>([
+  ["A", "Z"],
+  ["B", "X"],
+  ["C", "Y"],
+])
+
+const losses = new Map<choiceA, choiceB>([
+  ["A", "Y"],
+  ["B", "Z"],
+  ["C", "X"],
+])
+
+const draws = new Map<choiceA, choiceB>([
+  ["A", "X"],
+  ["B", "Y"],
+  ["C", "Z"],
+])
+
+export function scoreRound(opponent: choiceA, me: choiceB): number {
+  // I win
+  if (losses.get(opponent) === me) {
+    return points.get(me)! + 6
+  }
+
+  // they win
+  if (wins.get(opponent) === me) {
+    return points.get(me)!
+  }
+
+  // draw
+  return points.get(me)! + 3
+}
+
+export function getChoice(opponent: choiceA, instruction: choiceB): choiceB {
+  // They win
+  if (instruction === "X") {
+    return wins.get(opponent)!
+  }
+  // Draw
+  if (instruction === "Y") {
+    return draws.get(opponent)!
+  }
+  // I win
+  return losses.get(opponent)!
+}
+
+export function calculateTotalScoreWithInstruction(rounds: string[]): number {
+  return rounds
+    .map((round) => {
+      const [opponent, instruction] = round.split(" ") as [choiceA, choiceB]
+      return scoreRound(opponent, getChoice(opponent, instruction))
+    })
+    .reduce((sum, val) => (sum += val), 0)
+}
+
+export function calculateTotalScore(rounds: string[]): number {
+  return rounds
+    .map((round) => {
+      const [opponent, me] = round.split(" ") as [choiceA, choiceB]
+      return scoreRound(opponent, me)
+    })
+    .reduce((sum, val) => (sum += val), 0)
+}
+
 export default {
   partOne: () => {
-    return
+    return calculateTotalScore(
+      readFileSync("src/day02/input.txt").toString().split("\n")
+    )
   },
   partTwo: () => {
-    return
+    return calculateTotalScoreWithInstruction(
+      readFileSync("src/day02/input.txt").toString().split("\n")
+    )
   },
 }
