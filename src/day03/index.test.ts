a242fa5137843441ff7fe1da2f8bd405131bd488
A. Fox <anthonyfox1988@gmail.com>
2022-12-03 01:26:59 +0000

    day3 structure

---
+++ src/day03/index.test.ts
@@ -0,0 +1,5 @@
+describe("day03", () => {
+  test("part 1", () => {
+    expect(true).toBe(false)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import day01 from "./day01"
 import day02 from "./day02"
+import day03 from "./day03"
 
 console.table([
   { day: 1, partOne: day01.partOne(), partTwo: day01.partTwo() },
   { day: 2, partOne: day02.partOne(), partTwo: day02.partTwo() },
-  // more days are added here
+  { day: 3, partOne: day03.partOne(), partTwo: day03.partTwo() },
 ])
