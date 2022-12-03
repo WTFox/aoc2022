089885b5552eb6ffd942a66efc3c04ed00a1fc7b
A. Fox <anthonyfox1988@gmail.com>
2022-12-03 08:16:05 +0000

    day 4 structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import day01 from "./day01"
 import day02 from "./day02"
 import day03 from "./day03"
+import day04 from "./day04"
 
 console.table([
   { day: 1, partOne: day01.partOne(), partTwo: day01.partTwo() },
   { day: 2, partOne: day02.partOne(), partTwo: day02.partTwo() },
   { day: 3, partOne: day03.partOne(), partTwo: day03.partTwo() },
+  { day: 3, partOne: day04.partOne(), partTwo: day04.partTwo() },
 ])
