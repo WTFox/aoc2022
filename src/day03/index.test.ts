3d83b7f5122feaad5408dfa32f82be6c86012cb2
A. Fox <anthonyfox1988@gmail.com>
2022-12-03 08:10:28 +0000

    day 3

--- src/day03/index.test.ts
+++ src/day03/index.test.ts
@@ -1,5 +1,20 @@
+import { process, processThree } from "."
+
 describe("day03", () => {
+  const testInput = `\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+`.trim()
+
   test("part 1", () => {
-    expect(true).toBe(false)
+    expect(process(testInput)).toEqual(157)
+  })
+
+  test("part 2", () => {
+    expect(processThree(testInput)).toEqual(70)
   })
 })
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,8 +1,49 @@
+import { readFileSync } from "fs"
+
+export function intersection(first: string, second: string): Set<string> {
+  return new Set([...new Set(first)].filter((x) => new Set(second).has(x)))
+}
+
+export function getBadgePriority(x: string): number {
+  let charCode: number = x.charCodeAt(0) - 38
+  if (x === x.toLowerCase()) {
+    return (charCode -= 58)
+  }
+  return charCode
+}
+
+export function process(input: string): number {
+  let sum = 0
+  input.split("\n").map((val) => {
+    const first = val.slice(0, val.length / 2)
+    const second = val.slice(val.length / 2)
+    intersection(first, second).forEach((x) => {
+      sum += getBadgePriority(x)
+    })
+  })
+  return sum
+}
+
+export function processThree(input: string): number {
+  const types = []
+  const values = input.split("\n")
+  for (let index = 0; index < values.length - 2; index += 3) {
+    types.push(
+      Array.from(new Set([...values[index]!])).filter((x) =>
+        intersection(values[index + 1]!, values[index + 2]!).has(x)
+      )[0]!
+    )
+  }
+  return types.reduce((acc, val) => {
+    return (acc += getBadgePriority(val))
+  }, 0)
+}
+
 export default {
   partOne: () => {
-    return
+    return process(readFileSync("src/day03/input.txt", "utf8").toString())
   },
   partTwo: () => {
-    return
+    return processThree(readFileSync("src/day03/input.txt", "utf8").toString())
   },
 }
