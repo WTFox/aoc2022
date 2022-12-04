ddbc0f807b5ca5c6fda8830ca2413e90256f5d4a
A. Fox <anthonyfox1988@gmail.com>
2022-12-04 04:56:37 +0000

    add day template

---
+++ src/__day__/index.test.ts
@@ -0,0 +1,12 @@
+describe("day04", () => {
+  const testInput = `\
+`.trim()
+
+  test("part 1", () => {
+    expect(true).toBe(false)
+  })
+
+  test("part 2", () => {
+    expect(true).toBe(false)
+  })
+})
---
+++ src/__day__/index.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs"
+
+export default {
+  partOne: () => {
+    return readFileSync("src/day04/input.txt", "utf8").toString()
+  },
+  partTwo: () => {
+    return readFileSync("src/day04/input.txt", "utf8").toString()
+  },
+}
