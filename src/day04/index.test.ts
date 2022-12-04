f6eb52b7a74fad112d12a4eebf8838f726c740f0
A. Fox <anthonyfox1988@gmail.com>
2022-12-04 05:31:00 +0000

    day 4 - brute force

--- src/day04/index.test.ts
+++ src/day04/index.test.ts
@@ -1,12 +1,22 @@
+import { countEntireOverlaps, countOverlaps } from "."
+
 describe("day04", () => {
   const testInput = `\
-`.trim()
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+2-6,4-8 
+6-6,4-6
+`
+    .trim()
+    .split("\n")
 
   test("part 1", () => {
-    expect(true).toBe(false)
+    expect(countEntireOverlaps(testInput)).toEqual(2)
   })
 
   test("part 2", () => {
-    expect(true).toBe(false)
+    expect(countOverlaps(testInput)).toEqual(4)
   })
 })
--- src/day04/index.ts
+++ src/day04/index.ts
@@ -1,10 +1,82 @@
 import { readFileSync } from "fs"
 
+function range(start: string, end: string): number[] {
+  var ans = []
+  for (let i = parseInt(start); i <= parseInt(end); i++) {
+    ans.push(i)
+  }
+  return ans
+}
+
+export function countEntireOverlaps(input: string[]): number {
+  let count = 0
+  input.forEach((val) => {
+    if (!val) {
+      return
+    }
+    const [first, second] = val.split(",")
+    const firstRange = range(first!.split("-")[0]!, first!.split("-")[1]!)
+    const secondRange = range(second!.split("-")[0]!, second!.split("-")[1]!)
+
+    if (first === second) {
+      ++count
+      return
+    }
+
+    const largerArray =
+      firstRange.length > secondRange.length ? firstRange : secondRange
+    const smallerArray =
+      firstRange.length > secondRange.length ? secondRange : firstRange
+
+    count +=
+      largerArray.filter((val) => {
+        return [...smallerArray].includes(val)
+      }).length === smallerArray.length
+        ? 1
+        : 0
+  })
+  return count
+}
+
+export function countOverlaps(input: string[]): number {
+  let count = 0
+  input.forEach((val) => {
+    if (!val) {
+      return
+    }
+    const [first, second] = val.split(",")
+    const firstRange = range(first!.split("-")[0]!, first!.split("-")[1]!)
+    const secondRange = range(second!.split("-")[0]!, second!.split("-")[1]!)
+
+    if (first === second) {
+      ++count
+      return
+    }
+
+    const largerArray =
+      firstRange.length > secondRange.length ? firstRange : secondRange
+    const smallerArray =
+      firstRange.length > secondRange.length ? secondRange : firstRange
+
+    count +=
+      largerArray.filter((val) => {
+        return [...smallerArray].includes(val)
+      }).length > 0
+        ? 1
+        : 0
+  })
+  return count
+}
+
 export default {
   partOne: () => {
-    return readFileSync("src/day04/input.txt", "utf8").toString()
+    return countEntireOverlaps(
+      readFileSync("src/day04/input.txt", "utf8").toString().split("\n")
+    )
   },
   partTwo: () => {
-    return readFileSync("src/day04/input.txt", "utf8").toString()
+    return countOverlaps(
+      readFileSync("src/day04/input.txt", "utf8").toString().split("\n")
+    )
   },
 }
