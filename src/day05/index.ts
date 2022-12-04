6599b58889612f80465755c6f1451c53d2ee3635
A. Fox <anthonyfox1988@gmail.com>
2022-12-04 05:31:52 +0000

    add day 5 structure

---
+++ src/day05/index.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs"
+
+export default {
+  partOne: () => {
+    return readFileSync("src/day05/input.txt", "utf8").toString()
+  },
+  partTwo: () => {
+    return readFileSync("src/day05/input.txt", "utf8").toString()
+  },
+}
