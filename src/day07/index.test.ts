e2f9b59df51223859e35580d42df8f506b890be4
A. Fox <anthonyfox1988@gmail.com>
2022-12-06 14:03:21 -0800

    day07 structure

---
+++ src/day07/index.test.ts
@@ -0,0 +1,12 @@
+describe("day07", () => {
+  const testInput = `\
+`.trim()
+
+  test("part 1", () => {
+    expect(true).toBe(false)
+  })
+
+  test("part 2", () => {
+    expect(true).toBe(false)
+  })
+})
---
+++ src/day07/index.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from "fs"
+
+export default {
+  partOne: () => {
+    return readFileSync("src/day07/input.txt", "utf8").toString()
+  },
+  partTwo: () => {
+    return readFileSync("src/day07/input.txt", "utf8").toString()
+  },
+}
