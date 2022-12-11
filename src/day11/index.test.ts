d973eb72e00eb6e63d9338bfcf32e18bf036122f
A. Fox <anthonyfox1988@gmail.com>
2022-12-11 15:59:28 -0800

    11.2

--- src/day11/index.test.ts
+++ src/day11/index.test.ts
@@ -1,10 +1,12 @@
-import { Monkey, partOne } from "."
+import { Monkey, process } from "."
 
+// lcm of 23, 19, 13, 17 is 5985
 const monkies = new Map<number, Monkey>([
   [
     0,
     new Monkey(
       [79, 98],
+      5985,
       (old: number) => old * 19,
       (item: number) => (item % 23 === 0 ? 2 : 3)
     ),
@@ -13,6 +15,7 @@ const monkies = new Map<number, Monkey>([
     1,
     new Monkey(
       [54, 65, 75, 74],
+      5985,
       (old: number) => old + 6,
       (item: number) => (item % 19 === 0 ? 2 : 0)
     ),
@@ -21,6 +24,7 @@ const monkies = new Map<number, Monkey>([
     2,
     new Monkey(
       [79, 60, 97],
+      5985,
       (old: number) => old * old,
       (item: number) => (item % 13 === 0 ? 1 : 3)
     ),
@@ -29,6 +33,7 @@ const monkies = new Map<number, Monkey>([
     3,
     new Monkey(
       [74],
+      5985,
       (old: number) => old + 3,
       (item: number) => (item % 17 === 0 ? 0 : 1)
     ),
@@ -37,6 +42,9 @@ const monkies = new Map<number, Monkey>([
 
 describe("day11", () => {
   test("part 1", () => {
-    expect(partOne(monkies, 20)).toBe(10605)
+    expect(process(monkies, 20, true)).toBe(10605)
+  })
+  test("part 2", () => {
+    expect(process(monkies, 10000, false)).toBe(2713310158)
   })
 })
--- src/day11/index.ts
+++ src/day11/index.ts
@@ -2,35 +2,49 @@ export class Monkey {
   public itemsInspected: number
   constructor(
     public items: number[],
+    public lcm: number,
     public operation: (old: number) => number,
-    public throwTo: (newItem: number) => number
+    public throwTo: (newItem: number) => number,
+    public doRelief: boolean = true
   ) {
     this.items = items
     this.itemsInspected = 0
     this.operation = operation
     this.throwTo = throwTo
+    this.doRelief = doRelief
   }
 
   public processItems(monkies: Map<number, Monkey>) {
     while (this.items.length > 0) {
-      ++this.itemsInspected
-      const item = this.items.shift() as number
-      const newItem = Math.floor(this.operation(item) / 3)
-      const newMonkey = this.throwTo(newItem)
-      monkies.get(newMonkey)?.items.push(newItem)
+      this.itemsInspected++
+      let worryLevel = this.items.shift() as number
+      worryLevel = this.operation(worryLevel)
+      if (this.doRelief) {
+        worryLevel = Math.floor(worryLevel / 3)
+      } else {
+        worryLevel = worryLevel % this.lcm
+      }
+      const newMonkey = this.throwTo(worryLevel)
+      monkies.get(newMonkey)?.items.push(worryLevel)
     }
   }
 }
 
-export function partOne(monkies: Map<number, Monkey>, rounds: number) {
+export function process(
+  monkies: Map<number, Monkey>,
+  rounds: number,
+  doRelief: boolean
+) {
+  monkies.forEach((monkey) => (monkey.doRelief = doRelief))
   for (let index = 0; index < rounds; index++) {
     monkies.forEach((monkey) => monkey.processItems(monkies))
   }
-  const itemsInspectedPerMonkey = Array.from(monkies.values())
+
+  return Array.from(monkies.values())
     .map((monkey) => monkey.itemsInspected)
     .sort((a, b) => b - a)
-
-  return itemsInspectedPerMonkey.slice(0, 2).reduce((a, b) => a * b)
+    .slice(0, 2)
+    .reduce((a, b) => a * b)
 }
 
 export default {
@@ -40,6 +54,7 @@ export default {
         0,
         new Monkey(
           [61],
+          9699690,
           (old: number) => old * 11,
           (item: number) => (item % 5 === 0 ? 7 : 4)
         ),
@@ -49,6 +64,7 @@ export default {
         1,
         new Monkey(
           [76, 92, 53, 93, 79, 86, 81],
+          9699690,
           (old: number) => old + 4,
           (item: number) => (item % 2 === 0 ? 2 : 6)
         ),
@@ -58,6 +74,7 @@ export default {
         2,
         new Monkey(
           [91, 99],
+          9699690,
           (old: number) => old * 19,
           (item: number) => (item % 13 === 0 ? 5 : 0)
         ),
@@ -67,7 +84,8 @@ export default {
         3,
         new Monkey(
           [58, 67, 66],
-          (old: number) => old * old,
+          9699690,
+          (old: number) => old,
           (item: number) => (item % 7 === 0 ? 6 : 1)
         ),
       ],
@@ -76,6 +94,7 @@ export default {
         4,
         new Monkey(
           [94, 54, 62, 73],
+          9699690,
           (old: number) => old + 1,
           (item: number) => (item % 19 === 0 ? 3 : 7)
         ),
@@ -85,6 +104,7 @@ export default {
         5,
         new Monkey(
           [59, 95, 51, 58, 58],
+          9699690,
           (old: number) => old + 3,
           (item: number) => (item % 11 === 0 ? 0 : 4)
         ),
@@ -94,6 +114,7 @@ export default {
         6,
         new Monkey(
           [87, 69, 92, 56, 91, 93, 88, 73],
+          9699690,
           (old: number) => old + 8,
           (item: number) => (item % 3 === 0 ? 5 : 2)
         ),
@@ -103,14 +124,96 @@ export default {
         7,
         new Monkey(
           [71, 57, 86, 67, 96, 95],
+          9699690,
           (old: number) => old + 7,
           (item: number) => (item % 17 === 0 ? 3 : 1)
         ),
       ],
     ])
-    return partOne(monkies, 20)
+    return process(monkies, 20, true)
   },
   partTwo: () => {
-    return "TODO"
+    const monkies = new Map<number, Monkey>([
+      [
+        0,
+        new Monkey(
+          [61],
+          9699690,
+          (old: number) => old * 11,
+          (item: number) => (item % 5 === 0 ? 7 : 4)
+        ),
+      ],
+
+      [
+        1,
+        new Monkey(
+          [76, 92, 53, 93, 79, 86, 81],
+          9699690,
+          (old: number) => old + 4,
+          (item: number) => (item % 2 === 0 ? 2 : 6)
+        ),
+      ],
+
+      [
+        2,
+        new Monkey(
+          [91, 99],
+          9699690,
+          (old: number) => old * 19,
+          (item: number) => (item % 13 === 0 ? 5 : 0)
+        ),
+      ],
+
+      [
+        3,
+        new Monkey(
+          [58, 67, 66],
+          9699690,
+          (old: number) => old * old,
+          (item: number) => (item % 7 === 0 ? 6 : 1)
+        ),
+      ],
+
+      [
+        4,
+        new Monkey(
+          [94, 54, 62, 73],
+          9699690,
+          (old: number) => old + 1,
+          (item: number) => (item % 19 === 0 ? 3 : 7)
+        ),
+      ],
+
+      [
+        5,
+        new Monkey(
+          [59, 95, 51, 58, 58],
+          9699690,
+          (old: number) => old + 3,
+          (item: number) => (item % 11 === 0 ? 0 : 4)
+        ),
+      ],
+
+      [
+        6,
+        new Monkey(
+          [87, 69, 92, 56, 91, 93, 88, 73],
+          9699690,
+          (old: number) => old + 8,
+          (item: number) => (item % 3 === 0 ? 5 : 2)
+        ),
+      ],
+
+      [
+        7,
+        new Monkey(
+          [71, 57, 86, 67, 96, 95],
+          9699690,
+          (old: number) => old + 7,
+          (item: number) => (item % 17 === 0 ? 3 : 1)
+        ),
+      ],
+    ])
+    return process(monkies, 10000, false)
   },
 }
